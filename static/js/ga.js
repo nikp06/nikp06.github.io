a727e8367b545a3afd59c38cf72b63e23d3e8c45
Niklas Petersen <niklaspetersen@MBP-von-Niklas.fritz.box>
2024-12-03 12:01:40 +0100

    google analytics bugfix

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,10 +1,20 @@
-// ga.js
-(function () {
+(function() {
+    // Create the script element to load gtag.js
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-QF82QQD9DC';
+    
+    // Append the script tag to the document head
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(script, firstScriptTag);
+  
+    // Initialize dataLayer and gtag function
     window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
+    function gtag() {
+      dataLayer.push(arguments);
+    }
   
     // Initialize Google Analytics
     gtag('js', new Date());
-    gtag('config', 'G-QF82QQD9DC'); // Replace with your actual Google Analytics ID
+    gtag('config', 'G-QF82QQD9DC');
   })();
-  
